4c0651297e802188221283c59241572fc0228987
Bruno Nishida <bruno.nishida.ds@gmail.com>
2022-04-26 15:45:18 +0900

    add InputPrice component

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -14,6 +14,7 @@ import {
   Upload,
   PickImageButton
 } from './styles';
+import { InputPrice } from '@components/InputPrice';
 
 export function Product() {
   const [image, setImage] = useState('');
@@ -52,6 +53,10 @@ export function Product() {
           onPress={handleImagePicker}
         />
       </Upload>
+
+      <InputPrice size="S" />
+      <InputPrice size="M" />
+      <InputPrice size="L" />
     </Container>
   );
 }
