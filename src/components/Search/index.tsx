0a390ec76d50db91753f76b4d39ea0cdd0266980
Bruno Nishida <bruno.nishida.ds@gmail.com>
2022-05-20 15:48:30 +0900

    add Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useTheme } from 'styled-components/native';
+
+import {
+  Container,
+  Button,
+  ButtonClear,
+  Input,
+  InputArea,
+} from './styles';
+
+type SearchProps = TextInputProps & {
+  onSearch: () => void;
+  onClear: () => void;
+};
+
+export function Search({ onClear, onSearch, ...rest }: SearchProps) {
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <InputArea>
+        <Input placeholder="Search..."  {...rest} />
+
+        <ButtonClear onPress={onClear}>
+          <Feather name="x" size={16} />
+        </ButtonClear>
+      </InputArea>
+
+      <Button onPress={onSearch}>
+        <Feather name="search" size={16} color={COLORS.TITLE} />
+      </Button>
+
+    </Container>
+  );
+}
