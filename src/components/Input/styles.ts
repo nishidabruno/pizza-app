464496a7dbcef06c0b41dbf82da4bf5346e6d945
Bruno Nishida <bruno.nishida.ds@gmail.com>
2022-05-20 15:44:51 +0900

    fix input padding issue

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -17,7 +17,6 @@ export const Container = styled(TextInput).attrs<Props>(({ theme, type }) => ({
   background-color: transparent;
   border-radius: 12px;
   font-size: 14px;
-  padding: 7px 0;
   padding-left: 20px;
   margin-bottom: 16px;
 
